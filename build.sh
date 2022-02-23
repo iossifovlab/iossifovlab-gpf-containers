57985dd6a1dc6f4e2bb955424a56079b7bab287c
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-02-23 11:57:10 +0200

    GPF-1483 reorganizatio of gpf containers

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ function main() {
   local generate_jenkins_init="${options["generate_jenkins_init"]}"
   local expose_ports="${options["expose_ports"]}"
 
-  libmain_init seqpipe.seqpipe-gpf-containers sgc
+  libmain_init iossifovlab.iossifovlab-gpf-containers sgc
   libmain_init_build_env \
     clobber:"$clobber" preset:"$preset" build_no:"$build_no" \
     generate_jenkins_init:"$generate_jenkins_init" expose_ports:"$expose_ports" \
@@ -47,9 +47,7 @@ function main() {
     build_run_ctx_init "container" "ubuntu:20.04"
     defer_ret build_run_ctx_reset
     build_run rm -rf \
-      ./seqpipe-gpfjs/gpfjs
-    build_run rm -rf \
-      ./seqpipe-gpf/gpf
+      ./iossifovlab-gpf/gpf
   }
 
   local gpf_package_image
@@ -58,43 +56,43 @@ function main() {
   local gpfjs_package_image
   gpfjs_package_image=$(e docker_data_img_gpfjs_package)
 
-  build_stage "Build seqpipe-http"
+  build_stage "Build iossifovlab-http"
   {
-    build_docker_image_create "seqpipe-http" "seqpipe-http" "seqpipe-http/Dockerfile" "latest"
+    build_docker_image_create "iossifovlab-http" "iossifovlab-http" "iossifovlab-http/Dockerfile" "latest"
   }
 
-  build_stage "Build seqpipe-gpf"
+  build_stage "Build iossifovlab-gpf"
   {
     # copy gpf package
-    build_run_local mkdir -p ./seqpipe-gpf/gpf
-    build_docker_image_cp_from "$gpf_package_image" ./seqpipe-gpf/ /gpf
+    build_run_local mkdir -p ./iossifovlab-gpf/gpf
+    build_docker_image_cp_from "$gpf_package_image" ./iossifovlab-gpf/ /gpf
 
 
-    build_docker_image_create "seqpipe-gpf" "seqpipe-gpf" \
-      "seqpipe-gpf/Dockerfile" "no_tag"
+    build_docker_image_create "iossifovlab-gpf" "iossifovlab-gpf" \
+      "iossifovlab-gpf/Dockerfile" "no_tag"
   }
 
   build_stage "Build gpf-full"
   {
 
     # copy gpfjs package
-    build_run_local mkdir -p ./seqpipe-gpf-full/gpfjs
-    build_docker_image_cp_from "$gpfjs_package_image" ./seqpipe-gpf-full/ /gpfjs
+    build_run_local mkdir -p ./iossifovlab-gpf-full/gpfjs
+    build_docker_image_cp_from "$gpfjs_package_image" ./iossifovlab-gpf-full/ /gpfjs
 
 
     build_run_ctx_init "local"
     defer_ret build_run_ctx_reset
 
-    build_run cd seqpipe-gpf-full
+    build_run cd iossifovlab-gpf-full
 
     local docker_repo
     docker_repo=$(ee docker_repo)
 
-    local docker_img_seqpipe_gpf_tag
-    docker_img_seqpipe_gpf_tag=$(e docker_img_seqpipe_gpf_tag)
+    local docker_img_iossifovlab_gpf_tag
+    docker_img_iossifovlab_gpf_tag=$(e docker_img_iossifovlab_gpf_tag)
 
-    build_docker_image_create "seqpipe-gpf-full" "seqpipe-gpf-full" \
-      ./seqpipe-gpf-full/Dockerfile "${docker_img_seqpipe_gpf_tag}"
+    build_docker_image_create "iossifovlab-gpf-full" "iossifovlab-gpf-full" \
+      ./seqpipe-gpf-full/Dockerfile "${docker_img_iossifovlab_gpf_tag}"
   }
 }
 
