7dc932e37be46ebdd1f60f3c6e779d156c5ba349
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-09-22 11:49:09 +0300

    Increase the timeout for gunicorn fix.

--- iossifovlab-gpf-full/bin/start_gunicorn.sh
+++ iossifovlab-gpf-full/bin/start_gunicorn.sh
@@ -6,7 +6,7 @@
     --workers=1 \
     --threads=8 \
     --bind=0.0.0.0:9001 \
-    --timeout=300 \
+    --timeout=1200 \
     --access-logfile /logs/access.log \
     --error-logfile /logs/error.log \
     wdae.gunicorn_wsgi:application
