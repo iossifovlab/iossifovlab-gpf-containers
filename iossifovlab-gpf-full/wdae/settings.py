1c6a09917122dab467b583f78119d776d911bc3a
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2025-07-04 11:32:23 +0300

    Fix the Django static root setting

--- iossifovlab-gpf-full/wdae/settings.py
+++ iossifovlab-gpf-full/wdae/settings.py
@@ -3,3 +3,5 @@ import os
 from .default_settings import *  # type: ignore
 
 STUDIES_EAGER_LOADING = False
+
+STATIC_ROOT = '/site/gpf/static'
--- iossifovlab-gpf/wdae/settings.py
+++ iossifovlab-gpf/wdae/settings.py
@@ -5,3 +5,5 @@ from .default_settings import *  # type: ignore
 
 
 STUDIES_EAGER_LOADING = False
+
+STATIC_ROOT = '/site/gpf/static'
